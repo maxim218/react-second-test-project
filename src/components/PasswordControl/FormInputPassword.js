d25d4cc39f83c3097f41b9678e3ebb45e99d76ba
maxim218 <maxim.kolotovkin@yandex.ru>
2022-04-27 18:19:10 +0300

    use effect with dependencies

--- src/components/PasswordControl/FormInputPassword.js
+++ src/components/PasswordControl/FormInputPassword.js
@@ -1,8 +1,10 @@
 import {useState} from "react";
 import React from 'react';
+import {useEffect} from "react";
 
 export default function FormInputPassword(props) {
     const [a, setA] = useState("");
+    const [len, setLen] = useState(0);
 
     function handlerChangeA(aEvent) {
         setA(aEvent.target.value);
@@ -12,10 +14,19 @@ export default function FormInputPassword(props) {
         props.onInpOkBtnClick(a.trim());
     }
 
+    useEffect(() => {
+        const integerLength = a.trim().length;
+        console.log("integerLength: " + integerLength);
+        setLen(integerLength);
+    }, [a]);
+
     return (
         <React.Fragment>
             <p>Input secret password</p>
             <input type={"text"} spellCheck={"false"} autoComplete={"off"} onChange={handlerChangeA} value={a} />
+            <br/>
+            <br/>
+            Length: {len}
             <br/>
             <br/>
             <span onClick={handlerBtnClick} style={{cursor: 'pointer', color: 'blue', textDecoration: 'underline'}}>Log In</span>
